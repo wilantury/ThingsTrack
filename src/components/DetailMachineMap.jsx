02a418ae39373adca3da13752c574ef41abc3a5e
wilantury <ventas@antury.net>
2019-11-22 15:47:13 -0500

    getCurrentLocation from navigator, put currentLocation marker: done

--- src/components/DetailMachineMap.jsx
+++ src/components/DetailMachineMap.jsx
@@ -3,10 +3,43 @@ import {Map, InfoWindow, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 const DetailMachineMap = ({ google , locations}) => { 
 
+  function success(pos) {
+    var crd = pos.coords;
+
+    console.log('Your current position is:');
+    console.log('Latitude : ' + crd.latitude);
+    console.log('Longitude: ' + crd.longitude);
+    console.log('More or less ' + crd.accuracy + ' meters.');
+
+    setCurrent({
+      isLocated: true,
+      lng: crd.longitude,
+      lat: crd.latitude,
+    });
+  }
+
+  function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+  };
+
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+
+  navigator.geolocation.getCurrentPosition(success, error, options);
+  
   const [windowInfo, setValues] = useState({
     showingInfoWindow: false,
     activeMarker: {},
     selectedPlace: {},
+  })
+
+  const [currentLocation, setCurrent] = useState({
+    isLocated: false,
+    lng:{},
+    lat: {},
   })
 
   function onMarkerClick(props, marker, e) {
@@ -19,7 +52,7 @@ const DetailMachineMap = ({ google , locations}) => {
   return (
     <Map
       google={google}
-      zoom={11}
+      zoom={12}
       initialCenter={{ lat: locations[0].lat_location, lng: locations[0].lng_location }}
     >
         {
@@ -33,7 +66,14 @@ const DetailMachineMap = ({ google , locations}) => {
               />
             )
         }
-
+        {currentLocation.isLocated && 
+          <Marker
+            name={'home'}
+            title={'my location'}
+            onClick={onMarkerClick}
+            position={{ lat: currentLocation.lat, lng: currentLocation.lng}}
+          />
+        }
         <InfoWindow
           marker={windowInfo.activeMarker}
           visible={windowInfo.showingInfoWindow}>
